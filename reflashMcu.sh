e028bd3afa4b73a246e6256ab4777ad0c6f77aae
Ye Zhang <mr.yezhang@gmail.com>
2021-04-10 16:46:35 -0700

    Upgrade MicroPython firmware to v1.14.

--- reflashMcu.sh
+++ reflashMcu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cp boot.py ../.
-# curl -o .esp8266firmware.bin http://micropython.org/resources/firmware/esp8266-20191220-v1.12.bin
-curl -o .esp8266firmware.bin https://micropython.org/resources/firmware/esp8266-20200911-v1.13.bin
+curl -o .esp8266firmware.bin https://micropython.org/resources/firmware/esp8266-20210202-v1.14.bin
 esptool.py --port /dev/ttyUSB0 erase_flash && esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 .esp8266firmware.bin && rshell -p /dev/ttyUSB0 --buffer-size=30
